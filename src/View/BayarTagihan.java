e09c9124943a28d02d01938be265a5ea35ccbd72
asus <asus@asus-PC>
2021-11-24 20:51:55 +0700

    Perpendek import javax swing

--- src/View/BayarTagihan.java
+++ src/View/BayarTagihan.java
@@ -9,14 +9,8 @@ import Controller.ControllerTransaksi;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import Model.*;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JRootPane;
-import javax.swing.WindowConstants;
 
 /**
  *
